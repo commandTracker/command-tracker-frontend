75e0c44b545b43b760d9d11f2c4c39c06e3c69d2
ChaJunWoo18 <wnsdnxla@naver.com>
2025-06-01 21:14:07 +0900

    chore: 프로젝트 초기 설정 - close #1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import MainPage from "@/pages/MainPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
