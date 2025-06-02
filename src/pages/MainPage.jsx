cf9434d0ac5d28bb6cc7a742fa663aa489b44529
cholulu99 <tttjdrudwkd0@naver.com>
2025-06-02 15:09:18 +0900

    chore: .eslintrc.cjs에 useTabs: false 추가 - close #5

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,32 +4,32 @@ import Button from "@/shared/components/Button";
 import { LuMousePointerClick } from "react-icons/lu";
 
 function MainPage() {
-	return (
-		<div className="flex flex-col items-center py-5 px-4 w-full">
-			<div className="flex gap-5">
-				<LuMousePointerClick className="h-16 w-10" />
-				<p className="flex justify-center text-4xl mb-10">
-					Command
-					<br />
-					Tracker
-				</p>
-			</div>
-			<div className="space-y-1 text-center mb-10">
-				<p className="text-gray-700 text-lg">
-					캐릭터의 동작을 분석하고, 커맨드 입력을 추적합니다.
-				</p>
-			</div>
-			<div className="flex gap-3 w-full">
-				<Input
-					type="email"
-					value="youtube url"
-					onChange="test"
-					placeholder="이메일을 입력하세요"
-				/>
-				<Button children="제출" onClick="테스트" />
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-col items-center py-5 px-4 w-full">
+      <div className="flex gap-5">
+        <LuMousePointerClick className="h-16 w-10" />
+        <p className="flex justify-center text-4xl mb-10">
+          Command
+          <br />
+          Tracker
+        </p>
+      </div>
+      <div className="space-y-1 text-center mb-10">
+        <p className="text-gray-700 text-lg">
+          캐릭터의 동작을 분석하고, 커맨드 입력을 추적합니다.
+        </p>
+      </div>
+      <div className="flex gap-3 w-full">
+        <Input
+          type="email"
+          value="youtube url"
+          onChange="test"
+          placeholder="이메일을 입력하세요"
+        />
+        <Button children="제출" onClick="테스트" />
+      </div>
+    </div>
+  );
 }
 
 export default MainPage;
