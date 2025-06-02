70450d880dad97b8425adedf768bd3be99d757b1
ChaJunWoo18 <wnsdnxla@naver.com>
2025-06-02 11:18:22 +0900

    style: 공통 Button 컴포넌트 style 수정 - close #3
    - tailwind config의 color 속성 제거
    - Button 컴포넌트 padding 수정

--- src/shared/components/Button.jsx
+++ src/shared/components/Button.jsx
@@ -1,8 +1,8 @@
 import PropTypes from "prop-types";
 
 const buttonClass = `
-  text-white text-lg py-3 min-w-32 w-auto
-  rounded bg-primary
+  text-white text-lg py-2 min-w-32 w-auto
+  rounded bg-[#F4A76B]
   hover:bg-[#E08345] hover:border-[#E08345]
   active:font-bold focus:outline-none
   transition-colors duration-200
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: {
-    colors: {
-      primary: "#F4A76B",
-      white: "#FFF",
-    },
-  },
+  theme: {},
   plugins: [],
 };
