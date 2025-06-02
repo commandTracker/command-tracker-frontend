40d7c1ec2799a4e8074f87639fc4d2d9858cfd6b
ChaJunWoo18 <105590071+ChaJunWoo18@users.noreply.github.com>
2025-06-02 13:09:33 +0900

    features: 공통 컴포넌트 구현 (#4)
    * feat: 공통 Button 컴포넌트 구현
    
    * feat: 공통 Input 컴포넌트 구현
    
    * feat: 공통 Modal 컴포넌트 구현
    
    * style: 공통 Button 컴포넌트 style 수정 - close #3
    
    - tailwind config의 color 속성 제거
    - Button 컴포넌트 padding 수정

---
+++ src/shared/components/Button.jsx
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+
+const buttonClass = `
+  text-white text-lg py-2 min-w-32 w-auto
+  rounded bg-[#F4A76B]
+  hover:bg-[#E08345] hover:border-[#E08345]
+  active:font-bold focus:outline-none
+  transition-colors duration-200
+`;
+
+function Button({ children, onClick, disabled = false }) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      disabled={disabled}
+      className={buttonClass}
+    >
+      {children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool.isRequired,
+};
+
+export default Button;
