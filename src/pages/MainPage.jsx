bff925526a8f9a5879c703a8056d9cfa14b22398
cholulu99 <tttjdrudwkd0@naver.com>
2025-06-02 15:58:08 +0900

    chore: main 브랜치와 병합
    * feat: 메인페이지 UI 구현
    
    * chore: .eslintrc.cjs에 useTabs: false 추가 - close #5
    
    * refactor: Input 컴포넌트의 type prop 제거 및 onChange prop 빈 화살표로  변경 - close #5

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,34 @@
+import React from "react";
+import Input from "@/shared/components/Input";
+import Button from "@/shared/components/Button";
+import { LuMousePointerClick } from "react-icons/lu";
+
 function MainPage() {
-  return <h1 className="text-red-500">Hello world!</h1>;
+  return (
+    <div className="flex flex-col items-center py-5 px-4 w-full">
+      <div className="flex gap-5">
+        <LuMousePointerClick className="h-16 w-10" />
+        <p className="flex justify-center text-4xl mb-10">
+          Command
+          <br />
+          Tracker
+        </p>
+      </div>
+      <div className="space-y-1 text-center mb-10">
+        <p className="text-gray-700 text-lg">
+          캐릭터의 동작을 분석하고, 커맨드 입력을 추적합니다.
+        </p>
+      </div>
+      <div className="flex gap-3 w-full">
+        <Input
+          value="youtube url"
+          onChange={() => {}}
+          placeholder="유튜브 링크를 입력하세요"
+        />
+        <Button onClick={() => {}}>제출</Button>
+      </div>
+    </div>
+  );
 }
 
 export default MainPage;
