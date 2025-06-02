5f9c367caeab229efeab4a59113ba5dfc3b8a977
ChaJunWoo18 <wnsdnxla@naver.com>
2025-06-02 17:02:53 +0900

    feat: 사용자 이메일 입력 모달 UI 구현 - close #7 #8

---
+++ src/features/videoSubmit/components/EmailInputForm.jsx
@@ -0,0 +1,24 @@
+import { GrFormPreviousLink } from "react-icons/gr";
+
+import Input from "@/shared/components/Input";
+
+function EmailInputForm() {
+  return (
+    <div>
+      <GrFormPreviousLink size="27" />
+      <div className="text-2xl mb-8 mt-4 font-mediumm">
+        결과를 받을 메일을 입력하세요
+      </div>
+      <div className="flex justify-evenly items-center mb-8 text-[#515151] ">
+        <Input
+          type="email"
+          value=""
+          onChange={() => {}}
+          placeholder="example@exam.com"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default EmailInputForm;
--- src/features/videoSubmit/components/VideoSubmitModal.jsx
+++ src/features/videoSubmit/components/VideoSubmitModal.jsx
@@ -1,13 +1,14 @@
 import Modal from "@/shared/components/Modal";
 
 import CharacterSelectFrom from "./CharacterSelectForm";
+import EmailInputForm from "./EmailInputForm";
 
 function VideoSubmitModal() {
   const step = 1;
 
   return (
     <Modal onClick={() => {}} buttonText={step === 1 ? "다음" : "제출"}>
-      <CharacterSelectFrom />
+      {step === 1 ? <CharacterSelectFrom /> : <EmailInputForm />}
     </Modal>
   );
 }
