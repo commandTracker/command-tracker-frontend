5d40540c59b2917f791ee2243670dbbb77dc497d
cholulu99 <tttjdrudwkd0@naver.com>
2025-06-05 17:42:55 +0900

    feat: 아이콘 클릭 시 뒤로가는 goToPreviousStep 구현 및 Input의 이메일 입력 값 상태 관리

--- src/features/videoSubmit/components/EmailInputForm.jsx
+++ src/features/videoSubmit/components/EmailInputForm.jsx
@@ -1,24 +1,37 @@
+import PropTypes from "prop-types";
 import { GrFormPreviousLink } from "react-icons/gr";
 
 import Input from "@/shared/components/Input";
 
-function EmailInputForm() {
+function EmailInputForm({ email, onEmailChange, goToPreviousStep }) {
   return (
     <div>
-      <GrFormPreviousLink size="27" />
+      <GrFormPreviousLink
+        size="27"
+        onClick={goToPreviousStep}
+        className="cursor-pointer"
+      />
       <div className="text-2xl mb-8 mt-4 font-mediumm">
         결과를 받을 이메일을 입력하세요
       </div>
       <div className="flex mb-8 text-[#515151]">
         <Input
           type="email"
-          value=""
-          onChange={() => {}}
+          value={email}
+          onChange={(e) => {
+            onEmailChange(e.target.value);
+          }}
           placeholder="example@exam.com"
         />
       </div>
     </div>
   );
 }
+
+EmailInputForm.propTypes = {
+  email: PropTypes.string.isRequired,
+  onEmailChange: PropTypes.func.isRequired,
+  goToPreviousStep: PropTypes.func.isRequired,
+};
 
 export default EmailInputForm;
