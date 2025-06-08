7fe6dbd8fb3e7f53cf05c47c72442ba8e9b352db
ChaJunWoo18 <wnsdnxla@naver.com>
2025-06-08 22:57:56 +0900

    feat: 메인페이지 제출 버튼 api 요청 기능 추가

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,40 @@
+import { useState } from "react";
+
+import axios from "axios";
 import { LuMousePointerClick } from "react-icons/lu";
+import { useNavigate } from "react-router-dom";
 
 import Button from "@/shared/components/Button";
+import ErrorModal from "@/shared/components/ErrorModal";
 import Input from "@/shared/components/Input";
 
 function MainPage() {
+  const [url, setUrl] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async () => {
+    try {
+      if (url.trim() === "") {
+        throw new Error("유튜브 링크를 입력해주세요.");
+      }
+
+      const response = await axios.post("/api/video", { youtubeUrl: url });
+
+      setUrl("");
+      navigate("/video_editor", {
+        state: {
+          videoSrc: response.data.download_url,
+          videoId: response.data.video_id,
+        },
+      });
+    } catch (err) {
+      const message = err.response?.data?.message || err.message;
+
+      setError(message);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center py-5 px-4 w-full">
       <div className="flex gap-5">
@@ -21,12 +52,13 @@ function MainPage() {
       </div>
       <div className="flex gap-3 w-full">
         <Input
-          value="youtube url"
-          onChange={() => {}}
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
           placeholder="유튜브 링크를 입력하세요"
         />
-        <Button onClick={() => {}}>제출</Button>
+        <Button onClick={handleSubmit}>제출</Button>
       </div>
+      {error && <ErrorModal onClick={() => setError("")} message={error} />}
     </div>
   );
 }
