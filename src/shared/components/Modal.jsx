6e95b9c02eca5b701cceefe2eeef65ae598c868a
ChaJunWoo18 <wnsdnxla@naver.com>
2025-06-09 10:25:57 +0900

    refactor: arrow function으로 수정

--- src/shared/components/Modal.jsx
+++ src/shared/components/Modal.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 import Button from "./Button";
 
-function Modal({ children, onClick, buttonText }) {
+const Modal = ({ children, onClick, buttonText }) => {
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-slate-200 bg-opacity-50">
       <div className="py-8 px-6 bg-white rounded-md">
@@ -11,7 +11,7 @@ function Modal({ children, onClick, buttonText }) {
       </div>
     </div>
   );
-}
+};
 
 Modal.propTypes = {
   children: PropTypes.node.isRequired,
