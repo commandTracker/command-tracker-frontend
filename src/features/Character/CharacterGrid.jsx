77eb9f9a330b538b4cf0fd35ff8a9adb2515df97
cholulu99 <tttjdrudwkd0@naver.com>
2025-06-15 20:05:44 +0900

    feat: 캐릭터 목록을 표시하는 그리드 컴포넌트 추가

---
+++ src/features/Character/CharacterGrid.jsx
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+
+const CharacterGrid = ({ list }) => (
+  <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 gap-3">
+    {list.map(({ name, src }) => (
+      <figure
+        key={name}
+        className="flex flex-col items-center text-xs font-medium text-gray-700"
+      >
+        <img
+          src={src}
+          alt={name}
+          className="w-14 h-14 rounded object-cover shadow"
+        />
+        <figcaption className="truncate w-14 text-center mt-1">
+          {name}
+        </figcaption>
+      </figure>
+    ))}
+  </div>
+);
+
+CharacterGrid.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      src: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default CharacterGrid;
