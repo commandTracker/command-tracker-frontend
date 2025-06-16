5adb4ac789a3759f15e44089c597b1b1da4151d1
cholulu99 <tttjdrudwkd0@naver.com>
2025-06-16 19:30:42 +0900

    feat: 분석 요청 성공 페이지 라우팅 경로 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "@/pages/MainPage";
 import VideoEditor from "@/pages/VideoEditor";
 
+import SubmitSuccess from "./pages/SubmitSuccess";
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/video_editor" element={<VideoEditor />} />
+        <Route path="/submit_success" element={<SubmitSuccess />} />
       </Routes>
     </BrowserRouter>
   );
