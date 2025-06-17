56d9a4e58188f1c00f3341d1ce4728432181ec26
cholulu99 <tttjdrudwkd0@naver.com>
2025-06-17 17:54:29 +0900

    fix: 모달이 최상단에 있지 않아 겹치는 문제 해결 - close #34 (#35)

--- src/shared/components/LoadingModal.jsx
+++ src/shared/components/LoadingModal.jsx
@@ -1,6 +1,6 @@
 const LoadingModal = () => {
   return (
-    <div className="fixed inset-0 flex justify-center items-center bg-slate-200 bg-opacity-50">
+    <div className="fixed inset-0 z-50 flex justify-center items-center bg-slate-200 bg-opacity-50">
       <div className="rounded-md flex flex-col items-center">
         <div className="grid grid-cols-2 gap-2 mb-4">
           <div className="h-4 w-4 rounded-full bg-gray-400 animate-dot-flip [animation-delay:0ms]" />
